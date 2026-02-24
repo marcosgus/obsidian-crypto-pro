ef2c0f1258477fb6da74ff4dbde4619e380fdcda
marcosgus <68654936+marcosgus@users.noreply.github.com>
2026-02-24 09:35:39 -0300

    Pequeña modificacion estetica

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p commitMsg="Introduce el mensaje del commit: "
+rem set /p commitMsg="Introduce el mensaje del commit: "
 
 echo [1/4] Compilando TypeScript...
 call npx tsc
@@ -12,17 +12,17 @@ if %ERRORLEVEL% NEQ 0 (
 echo [2/4] Actualizando main.js en la raiz...
 copy /Y dist\main.js .\main.js
 
-echo [3/4] Preparando cambios para GitHub...
-git add .
-git commit -m "%commitMsg%"
+rem echo [3/4] Preparando cambios para GitHub...
+rem git add .
+rem git commit -m "%commitMsg%"
 
-echo [4/4] Subiendo al repositorio remoto...
-git push origin main
+rem echo [4/4] Subiendo al repositorio remoto...
+rem git push origin main
 
-if %ERRORLEVEL% EQU 0 (
-    echo [EXITO] Compilado y subido correctamente a GitHub.
-) else (
-    echo [ERROR] No se pudo subir a GitHub. Verifica tu conexion o permisos.
-)
+rem if %ERRORLEVEL% EQU 0 (
+rem    echo [EXITO] Compilado y subido correctamente a GitHub.
+rem ) else (
+rem     echo [ERROR] No se pudo subir a GitHub. Verifica tu conexion o permisos.
+rem )
 
 pause
